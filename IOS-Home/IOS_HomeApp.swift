7da9a2e2001437ed3665e5eb96f85d282c614555
git-hariom <hariom.sinha.dev@gmail.com>
2024-12-24 13:17:45 +0530

    Initial Commit

---
+++ IOS-Home/IOS_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HomeApp.swift
+//  IOS-Home
+//
+//  Created by Hariom Sinha on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
